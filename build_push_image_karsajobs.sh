2e328befda16c52061158fae0471a5fbbd31abd0
andrianprasetya <andrianprasetya223@gmail.com>
2023-12-31 23:50:41 +0700

    add build and ci

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,11 @@
+echo 'Build Dockerfile to image'
+set -x
+docker build -t ghcr.io/andrianprasetya/karsajobs-frontend:latest  .
+set +x
+echo 'Login Github Packages'
+set -x
+echo $PASSWORD_GITHUB_PACKAGES | docker login ghcr.io -u andrianprasetya --password-stdin
+set +x
+echo 'Push Image'
+set -x
+docker push ghcr.io/andrianprasetya/karsajobs-frontend:latest
