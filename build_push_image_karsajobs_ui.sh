e093f64cc74a9927d6cb172960d1d83041f65764
andrianprasetya <andrianprasetya223@gmail.com>
2024-01-01 01:01:05 +0700

    add build and circleCi

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -3,7 +3,6 @@ set -x
 docker build -t ghcr.io/andrianprasetya/karsajobs-frontend:latest  .
 set +x
 echo 'Login Github Packages'
-echo "GitHub Packages Password: $PASSWORD_GITHUB_PACKAGES"
 set -x
 echo $PASSWORD_GITHUB_PACKAGES | docker login ghcr.io -u andrianprasetya --password-stdin
 set +x
